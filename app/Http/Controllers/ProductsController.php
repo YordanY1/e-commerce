e3ab7c86300491467c621f176d776f541b668ca0
YordanYordanov1948 <yordan_yordanov001@abv.bg>
2023-12-25 11:26:53 +0200

    price filters

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -10,19 +10,48 @@ class ProductsController extends Controller
 {
     public function index(Request $request)
     {
-        $categories = Category::all(); // Fetch all categories
+        $categories = Category::all();
 
+        $query = Product::query()->with(['images', 'price']);
+
+        // Category filter logic
         $categoryId = $request->query('category');
-
         if ($categoryId) {
-            $products = Product::whereHas('attributes', function ($query) use ($categoryId) {
+            $query->whereHas('attributes', function ($query) use ($categoryId) {
                 $query->whereJsonContains('categories', (string) $categoryId);
-            })->with(['images', 'price'])->get();
-        } else {
-            $products = Product::with(['images', 'price'])->get();
+            });
         }
 
+        // Price range filter logic
+        $priceRange = $request->query('priceRange');
+        switch ($priceRange) {
+            case 'price-range-1':
+                $query->whereHas('price', function($q) {
+                    $q->where('price', '<=', 50);
+                });
+                break;
+            case 'price-range-2':
+                $query->whereHas('price', function($q) {
+                    $q->whereBetween('price', [50, 100]);
+                });
+                break;
+            case 'price-range-3':
+                $query->whereHas('price', function($q) {
+                    $q->whereBetween('price', [100, 200]);
+                });
+                break;
+            case 'price-range-4':
+                $query->whereHas('price', function($q) {
+                    $q->where('price', '>', 200);
+                });
+                break;
+        }
+
+        $products = $query->get();
+
+        // Check if the request is an AJAX call
         if ($request->ajax()) {
+            // Return JSON response for AJAX request
             return response()->json([
                 'html' => view('products.partials.product_list', compact('products'))->render()
             ]);
@@ -30,6 +59,5 @@ class ProductsController extends Controller
 
         // Return the full view for non-AJAX requests
         return view('products.products', compact('products', 'categories'));
-
     }
 }

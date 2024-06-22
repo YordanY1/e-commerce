83c4cd42c9ff27a741f3875956511a1b25f215c4
YordanYordanov1948 <yordanyordanov0104@gmail.com>
2024-06-22 09:38:41 +0300

    all products logic

--- resources/views/products/category.blade.php
+++ resources/views/products/category.blade.php
@@ -8,6 +8,20 @@
     @if ($category->children->isNotEmpty())
         <h4>Подкатегории:</h4>
         <div class="row">
+            <!-- Card for "All" categories -->
+            <div class="col-md-4">
+                <div class="card mb-4">
+                    @if ($category->image) <!-- Checking if the category has an image -->
+                        <img src="{{ asset('storage/' . $category->image) }}" class="card-img-top" alt="Всички категории">
+                    @endif
+                    <div class="card-body">
+                        <h5 class="card-title">Всички</h5>
+                        <p class="card-text">Виж всички продукти от категорията</p>
+                        <a href="{{ url('/products?category=' . $category->slug) }}" class="btn btn-primary">Всички</a>
+                    </div>
+                </div>
+            </div>
+
             @foreach ($category->children as $subCategory)
                 <div class="col-md-4">
                     <div class="card mb-4">

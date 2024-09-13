caa7905a9bd86e3a3a0ba143aa81ec7e896721ab
YordanYordanov <yordanyordanov0104@gmail.com>
2024-09-13 15:46:57 +0300

    check

--- database/migrations/2023_09_13_099900_create_products_table.php
+++ database/migrations/2023_09_13_099900_create_products_table.php
@@ -9,13 +9,16 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
-        Schema::create('products', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
+        if (!Schema::hasTable('products')) {
+            Schema::create('products', function (Blueprint $table) {
+                $table->bigIncrements('id');
+                $table->timestamps();
+            });
+        }
     }
+
 
     /**
      * Reverse the migrations.
